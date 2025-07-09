1d980b7f093fb641b13773fc00367b0fbceefeb5
ancu-98 <jdsebastianb30@hotmail.com>
2025-07-09 13:23:13 -0500

    adding create_book view

--- Server/backend/crudapi/views.py
+++ Server/backend/crudapi/views.py
@@ -20,4 +20,4 @@ def create_book(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
-    return Response(serializer.error,  status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors,  status = status.HTTP_400_BAD_REQUEST)
