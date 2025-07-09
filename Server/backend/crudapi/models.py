633d17a12d6218b0efd17b2f18f798d442722a10
ancu-98 <jdsebastianb30@hotmail.com>
2025-07-09 13:22:25 -0500

    redefinign books model

--- Server/backend/crudapi/models.py
+++ Server/backend/crudapi/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=40)
+    autor = models.CharField(max_length=100)
     release_year = models.IntegerField()
 
     def __srt__(self):
         return self.title
+
+# {"title" : "You Don’t Know JS: Scope & Closures", "autor" : "Kyle Simpson", "release_year": 2014}
